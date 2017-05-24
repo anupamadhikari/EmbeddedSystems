228c42b27a94eec8471227a4e98e3a65fb0c3f65
Anupam Adhikari <19anupamadhikari97@gmail.com>
2017-05-24 21:23:06 +0530

    Add files via upload

---
+++ single esc/sketch_sep26c.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+ 
+Servo esc;
+int throttlePin = 9;
+ 
+void setup()
+{
+esc.attach(9);
+Serial.begin(9600);
+}
+ 
+void loop()
+{
+int throttle = Serial.read();
+throttle = map(throttle, 0, 1023, 0, 179);
+esc.write(throttle);
+}
